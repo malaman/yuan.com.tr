5edb9f27c05b7731034356ba26a875a6615b1ad0
Atila Satilmis <atila.satilmis@gmail.com>
2016-12-13 17:01:56 +0300

    blah

--- webapp/wsgi.py
+++ webapp/wsgi.py
@@ -13,6 +13,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webapp.settings.debug")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webapp.settings.dev")
 
 application = get_wsgi_application()

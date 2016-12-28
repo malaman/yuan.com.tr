278d0cb9656a435c0f2828f59b873900da992a74
Atila Satilmis <atila.satilmis@gmail.com>
2016-12-28 17:26:43 +0300

    balh

--- unify/templatetags/unify.py
+++ unify/templatetags/unify.py
@@ -8,11 +8,17 @@ def get_language_root(context):
     # so object-comparison to self will return false as objects would differ
     return context['self'].get_ancestors(inclusive=True).get(depth=3)
 
+def has_menu_children(page):
+    return page.get_children().live().in_menu().exists()
+
 # Retrieves the top menu items - the immediate children of the parent page
+# The has_menu_children method is necessary because the bootstrap menu requires
+# a dropdown class to be applied to a parent
 @register.inclusion_tag('tags/top_menu.html', takes_context=True)
 def top_menu(context, parent, calling_page=None):
     menuitems = parent.get_children().live().in_menu()
     for menuitem in menuitems:
+        menuitem.show_dropdown = has_menu_children(menuitem)
         # We don't directly check if calling_page is None since the template
         # engine can pass an empty string to calling_page
         # if the variable passed as calling_page does not exist.
@@ -24,4 +30,16 @@ def top_menu(context, parent, calling_page=None):
         # required by the pageurl tag that we want to use within this template
         'request': context['request'],
         'self': context['self'],
+    }
+
+# Retrieves the children of the top menu items for the drop downs
+@register.inclusion_tag('tags/top_menu_children.html', takes_context=True)
+def top_menu_children(context, parent):
+    menuitems_children = parent.get_children()
+    menuitems_children = menuitems_children.live().in_menu()
+    return {
+        'parent': parent,
+        'menuitems_children': menuitems_children,
+        # required by the pageurl tag that we want to use within this template
+        'request': context['request'],
     }

4520c245601b8aff7dee03a1b0e4f41d7ba67b8f
Atila Satilmis <atila.satilmis@gmail.com>
2016-12-29 11:48:48 +0300

    blah

--- home/models.py
+++ home/models.py
@@ -1,9 +1,73 @@
-from wagtail.wagtailcore.models import Page
-from wagtail.wagtailadmin.edit_handlers import MultiFieldPanel
+from django.db import models
+from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, \
+    MultiFieldPanel, PageChooserPanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtaildocs.edit_handlers import DocumentChooserPanel
+from modelcluster.fields import ParentalKey
 from language.link import TranslatablePageMixin
 
 
+class LinkFields(models.Model):
+    link_external = models.URLField("External link", blank=True)
+    link_page = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+    link_document = models.ForeignKey(
+        'wagtaildocs.Document',
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+
+    @property
+    def link(self):
+        if self.link_page:
+            return self.link_page.url
+        elif self.link_document:
+            return self.link_document.url
+        else:
+            return self.link_external
+
+    panels = [
+        FieldPanel('link_external'),
+        PageChooserPanel('link_page'),
+        DocumentChooserPanel('link_document'),
+    ]
+
+    class Meta:
+        abstract = True
+
+class CarouselItem(LinkFields):
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    embed_url = models.URLField("Embed URL", blank=True)
+    caption = models.CharField(max_length=255, blank=True)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('embed_url'),
+        FieldPanel('caption'),
+        MultiFieldPanel(LinkFields.panels, "Link"),
+    ]
+
+    class Meta:
+        abstract = True
+
+class HomePageCarouselItem(Orderable, CarouselItem):
+    page = ParentalKey('HomePage', related_name='carousel_items')
+
 class HomePage(Page, TranslatablePageMixin):
-    content_panels = Page.content_panels + [
+    content_panels = [
+        FieldPanel('title', classname="full title"),
+        InlinePanel('carousel_items', label="Carousel items"),
         MultiFieldPanel(TranslatablePageMixin.panels, 'Language links')
     ]
